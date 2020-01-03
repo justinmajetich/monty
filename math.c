25ede345a0fdbac4a3c11054aa64474a42ec6eb6
justinmajetich <justinmajetich@gmail.com>
2020-01-03 21:46:10 +0000

    Build support for mul and mod opcodes

--- math.c
+++ math.c
@@ -29,7 +29,6 @@ void _add(stack_t **head, unsigned int line_number)
  * @head: pointer to beginning of stack or queue
  * @line_number: line number of input file
  */
-
 void _sub(stack_t **head, unsigned int line_number)
 {
 	stack_t *a, *b;
@@ -51,29 +50,74 @@ void _sub(stack_t **head, unsigned int line_number)
 }
 
 /**
- * _div - divides top two elements of stack
+ * _div - divides second elem by top elem of stack
  * @head: pointer to beginning of stack or queue
  * @line_number: line number of input file
  */
-
 void _div(stack_t **head, unsigned int line_number)
 {
 	stack_t *a, *b;
 	(void)line_number;
 
 	if (node_count(head) < 2)
-		print_error(10, NULL);
+		print_error(11, NULL);
 
 	/* point a and b to appropriate nodes */
 	a = *head;
 	b = a->next;
 
 	if (a->n == 0) /* check for divide by 0 */
-		print_error(11, NULL);
-	b->n = b->n / a->n; /* assign sum to b node */
+		print_error(12, NULL);
+	b->n = b->n / a->n; /* assign division result to b node */
 	b->prev = NULL;
 	*head = b; /* adjust head pointer */
 
 	/* free top node */
+	free(a);
+}
+/**
+ * _mul - multiply top two elements in stack
+ * @head: pointer to beginning of stack or queue
+ * @line_number: line number of input file
+ */
+void _mul(stack_t **head, unsigned int line_number)
+{
+	stack_t *a, *b;
+	(void)line_number;
+
+	if (node_count(head) < 2)
+		print_error(13, NULL);
+
+	/* point a and b to appropriate nodes */
+	a = *head;
+	b = a->next;
+
+	b->n = a->n * b->n; /* assign product to b value */
+	b->prev = NULL;
+	*head = b;
+	free(a);
+}
+/**
+ * _mod - take remainder of second stack elem divided by top elem 
+ * @head: pointer to beginning of stack or queue
+ * @line_number: line number of input file
+ */
+void _mod(stack_t **head, unsigned int line_number)
+{
+	stack_t *a, *b;
+	(void)line_number;
+
+	if (node_count(head) < 2)
+		print_error(14, NULL);
+
+	/* point a and b to appropriate nodes */
+	a = *head;
+	b = a->next;
+
+	if (a->n == 0) /* check for divide by 0 */
+		print_error(12, NULL);
+	b->n = b->n % a->n;
+	b->prev = NULL;
+	*head = b;
 	free(a);
 }
--- monty.h
+++ monty.h
@@ -68,6 +68,8 @@ void _swap(stack_t **head, unsigned int line_number);
 void _nop(stack_t **head, unsigned int line_number);
 void _sub(stack_t **head, unsigned int line_number);
 void _div(stack_t **head, unsigned int line_number);
+void _mod(stack_t **head, unsigned int line_number);
+void _mul(stack_t **head, unsigned int line_number);
 
 /* UTILITY PROTOTYPES */
 size_t node_count(stack_t **h);
