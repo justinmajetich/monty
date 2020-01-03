ffbe593f189099ef21ee7314674c3939eb17c7c5
mmoscovics <mmoscovics@yahoo.com>
2020-01-03 16:06:54 +0000

    [Update] add pint opcode

--- print.c
+++ print.c
@@ -2,14 +2,31 @@
 
 /**
  * _pall - prints all values on the stack, starting from the top
- * @args: arguments of line passed in from file
+ * @head: pointer to beginning of stack or queue
+ * @line_number: the line number at which the opcode is called on
  */
 
 void _pall(stack_t **head, unsigned int line_number)
 {
+	stack_t *pos = *head;
 	int i;
 	(void)line_number;
 
-	for (i = 0; *head != NULL; i++, *head = (*head)->next)
-		printf("%d\n", (*head)->n);
+	for (i = 0; pos != NULL; i++, pos = pos->next)
+		printf("%d\n", pos->n);
+}
+
+/**
+ * _pint - prints the calue at the top of the stack
+ * @head: pointer to beginning of stack or queue
+ * @line_number: the line number at which the opcode is called on
+ */
+
+void _pint(stack_t **head, unsigned int line_number)
+{
+	(void)line_number;
+
+	if (*head == NULL)
+		print_error(6, NULL);
+	printf("%d\n", (*head)->n);
 }
